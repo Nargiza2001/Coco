c1ab2a3611baaa0f336d6eecd5ddb4fd630f5b01
дройдер <drojder@MacBook-Air-drojder.local>
2022-12-26 21:11:43 +0600

    Changed color of the background in ViewController to blue color

--- Coco/ViewController.swift
+++ Coco/ViewController.swift
@@ -28,7 +28,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .yellow
+        view.backgroundColor = .blue
         configureTitleLabel()
         configureNameButton()
     }
